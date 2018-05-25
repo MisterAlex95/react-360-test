c2247623ff7f65f25f6d794e80999271197ddc36
Alex Dana <alex.dana@epitech.eu>
2018-05-26 00:20:17 +0200

    add basic mouse

--- client.js
+++ client.js
@@ -1,12 +1,15 @@
 // This file contains the boilerplate to execute your React app.
 // If you want to modify your application's content, start in "index.js"
 
-import {ReactInstance} from 'react-360-web';
+import { ReactInstance } from 'react-360-web';
+import SimpleRaycaster from "simple-raycaster";
+
 
 function init(bundle, parent, options = {}) {
   const r360 = new ReactInstance(bundle, parent, {
     // Add custom options here
     fullScreen: true,
+    cursorVisibility: "visible",
     ...options,
   });
 
@@ -18,6 +21,8 @@ function init(bundle, parent, options = {}) {
 
   // Load the initial environment
   r360.compositor.setBackground(r360.getAssetURL('360_world.jpg'));
+  r360.controls.clearRaycasters();
+  r360.controls.addRaycaster(SimpleRaycaster);
 }
 
 window.React360 = {init};
