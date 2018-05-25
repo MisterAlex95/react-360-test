9813e7634319130de9babf018efa1c224393cd64
Alex Dana <alex.dana@epitech.eu>
2018-05-25 22:57:14 +0200

    initial push

---
+++ ecosystem.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    /**
+     * Application configuration section
+     * http://pm2.keymetrics.io/docs/usage/application-declaration/
+     */
+    apps : [],
+  
+    /**
+     * Deployment section
+     * http://pm2.keymetrics.io/docs/usage/deployment/
+     */
+    deploy : {
+    //   production : {
+    //     user : 'deploy',
+    //     host : 'lts1.milky.fr',
+    //     ref  : 'origin/master',
+    //     repo : 'git@github.com:misteralex95/react-360-test.git',
+    //     path : '/home/deploy/react-360-test',
+    //     'post-deploy' : 'source ~/.bashrc && yarn install && yarn run build',
+    //     env  : {
+    //       NODE_ENV: 'production',
+    //     }
+    //   },
+      staging : {
+        user : 'deploy',
+        host : 'dev.milky.fr',
+        ref  : 'origin/develop',
+        repo : 'git@github.com:misteralex95/react-360-test.git',
+        path : '/home/deploy/react-360-test',
+        'post-deploy' : '. ~/.bashrc && yarn install && pm2 reload ecosystem.config.js --env staging && yarn run build',
+        env  : {
+          NODE_ENV: 'development',
+        }
+      }
+    }
+  };
---
+++ index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-360';
+
+export default class test360 extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      textColor: 'white'
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.panel}>
+        <View style={styles.greetingBox}>
+          <Text style={styles.greeting}>
+            Welcome to React 360
+          </Text>
+          <Text
+          style={{color: this.state.textColor}}
+          onEnter={() => this.setState({textColor: 'red'})}
+          onExit={() => this.setState({textColor: 'white'})}>
+          This text will turn red when you look at it.
+        </Text>
+        </View>
+      </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  panel: {
+    // Fill the entire surface
+    width: 1000,
+    height: 600,
+    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  greetingBox: {
+    padding: 20,
+    backgroundColor: '#000000',
+    borderColor: '#639dda',
+    borderWidth: 2,
+  },
+  greeting: {
+    fontSize: 30,
+  },
+});
+
+AppRegistry.registerComponent('test360', () => test360);
