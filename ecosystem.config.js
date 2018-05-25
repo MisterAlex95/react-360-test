3d433c762ee29f74fe2628d1da9705744c931f25
Alex Dana <alex.dana@epitech.eu>
2018-05-26 00:09:38 +0200

    final config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -27,7 +27,7 @@ module.exports = {
         ref  : 'origin/develop',
         repo : 'https://github.com/MisterAlex95/react-360-test.git',
         path : '/home/deploy/react-360-test',
-        'post-deploy' : '. ~/.bashrc && yarn install && pm2 reload ecosystem.config.js --env staging && yarn run bundle',
+        'post-deploy' : '. ~/.bashrc && yarn install && pm2 reload ecosystem.config.js --env staging && yarn run bundle && cp -r ./static_assets ./build/static_assets',
         env  : {
           NODE_ENV: 'development',
         }
