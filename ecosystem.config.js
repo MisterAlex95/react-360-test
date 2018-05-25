0fdc0f235168d2613cc0c10a737390a22655e517
Alex Dana <alex.dana@epitech.eu>
2018-05-25 23:54:42 +0200

    remove apps

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,16 +3,7 @@ module.exports = {
      * Application configuration section
      * http://pm2.keymetrics.io/docs/usage/application-declaration/
      */
-    apps : [
-        {
-            name      : 'react-360-test',
-            script    : 'yarn',
-            args      : 'run start:staging',
-            env_production : {
-              NODE_ENV: 'staging'
-            }
-        }
-    ],
+    apps : [],
   
     /**
      * Deployment section
